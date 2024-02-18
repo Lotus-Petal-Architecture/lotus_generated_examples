a360052ae51aca86a02f9f6649eec14675bf5997
tessgadwa <tessgadwa@gmail.com>
2024-02-17 21:27:05 -0500

    print title row of test CSV

--- lotus3D/financial_source/python/data_import.py
+++ lotus3D/financial_source/python/data_import.py
@@ -4,6 +4,14 @@ import pandas as pandasForSortingCSV
 # assign dataset
 csvData = pandasForSortingCSV.read_csv("../data/data.csv")
 
+# creating a list of column names by
+# calling the .columns
+list_of_column_names = list(csvData.columns)
+
+# displaying the list of column names
+print('List of column names : ',
+      list_of_column_names)
+
 # displaying unsorted data frame
 print("\nBefore sorting:")
 print(csvData)

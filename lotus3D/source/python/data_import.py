3ade8b7ccc567c09f8ad00c86a0711f89c018efe
tessgadwa <tessgadwa@gmail.com>
2024-02-23 16:00:53 -0500

    define sort data frame function

--- lotus3D/source/python/data_import.py
+++ lotus3D/source/python/data_import.py
@@ -4,11 +4,12 @@ import pandas as pandasForSortingCSV
 # assign dataset
 csvData = pandasForSortingCSV.read_csv('../data/data.csv')
 
-# creating a list of column names by
-# calling the .columns
+# creating a list of column names by calling the .columns
 list_of_column_names = list(csvData.columns)
+column_count = len(list_of_column_names)
 
-# displaying the list of column names
+# displaying the list and number of column names
+print('Number of columns :', column_count)
 print('List of column names : ',
       list_of_column_names)
 
@@ -17,11 +18,16 @@ print("\nBefore sorting:")
 print(csvData)
 
 # sort data frame
-# csvData.sort_values(["Previous Year"],
-#                    axis=0,
-#                    ascending=[False],
-#                    inplace=True)
 
-# displaying sorted data frame
-# print("\nAfter sorting:")
-# print(csvData)
+def sortframe(colname):
+    csvData.sort_values(colname,
+                    axis=0,
+                    ascending=[False],
+                    inplace=True)
+
+    print("\nAfter sorting:")
+    print(csvData)
+
+colname = "Expenditure"
+
+sortframe(colname)

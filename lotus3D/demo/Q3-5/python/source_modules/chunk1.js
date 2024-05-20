5f5bb16cdc3a2d85b0816a2dee5eed7f82c0012b
Elizabeth Gadwa <tessgadwa@gmail.com>
2024-05-19 22:14:07 -0700

    include remaining Lotus Charts for question categories

---
+++ lotus3D/demo/Q3-5/python/source_modules/chunk1.js
@@ -0,0 +1,52 @@
+//Copyright 2024 by Lotus.fm LLC
+
+var camera, scene, raycaster, renderer, parentTransform
+var mouse = new THREE.Vector2()
+var r = 100,
+  dot = 0
+
+  var scene = new THREE.Scene()
+
+  var camera = new THREE.PerspectiveCamera(  
+    7.5, // This variable controls size -- the lower the value the larger the rendering. Original value was 27.
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  )
+
+  var container = document.getElementById('container')
+
+  containerWidth = window.innerWidth
+  containerHeight = window.innerHeight
+
+  renderer = new THREE.WebGLRenderer({ antialias: true })
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.body.appendChild(renderer.domElement)
+
+  var controls = new THREE.OrbitControls(camera, renderer.domElement)
+  controls.minDistance = 0
+  controls.maxDistance = 100
+  controls.maxPolarAngle = Math.PI / 2
+  controls.addEventListener( 'change', () => renderer.render( scene, camera ) );
+
+
+  var light = new THREE.PointLight(0xffffff)
+  light.position.set(-100, 200, 100)
+  scene.add(light)
+
+  var group
+  group = new THREE.Group()
+  group.position.set( 0, -.08, -5);
+  group.rotation.set(1,0,0);
+  scene.add(group)
+
+  var link_order_length = 0
+  var link_order = [] // list of all link values in the module, with k values assigned to each index position
+  var k_values = [] // list of all k values generated for corresponding module chart lines
+  var active_links = [] //index values of active links
+  var active_links2 = [] //index values of active links
+
+
+//dynamically generated code is inserted here
+
